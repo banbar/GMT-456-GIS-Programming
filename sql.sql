d3823fd86456ed2322b3a93b1c0333c581ccc245
banbar <banbar@hacettepe.edu.tr>
2019-02-12 23:14:03 +0300

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,23 +1,26 @@
-﻿-- CREATE THE POSTGIS EXTENSION
+﻿
+-- Create the PostGIS extension
 create extension postgis
 
 -- Import the polygons shapefile
 
--- CREATING THE NECESSARY TABLES
+-- Creating the centroid & edges tables & populating them 
+
+--Centroid
 
 -- NODES - City Centroids
 	-- Create the table of centroids 
 	drop table public.centroids
 	CREATE TABLE public.centroids (
 	    gid         integer CONSTRAINT firstkey PRIMARY KEY,
-	    name_polygon   varchar(40) NOT NULL,
+	    name   varchar(40) NOT NULL,
 	    geom	geometry(point,3857)
 	);
 
 	-- Insert the centroids to the new table
 	insert into public.centroids
-		(select gid, name, st_centroid(geom) 
-		from polygons)
+		(select gid, ad, st_setSRID(st_centroid(geom),3857) 
+		from p_iller)
 
 -- EDGES - Lines between adjacent polygons
 
@@ -37,8 +40,8 @@ create extension postgis
 	insert into public.edges(origin, destination, weight, geom, origin_gid, destination_gid)
 		(select a.ad, b.ad,  
 			(st_length(st_makeline(st_centroid(a.geom), st_centroid(b.geom)))),
-			st_makeline(st_centroid(a.geom), st_centroid(b.geom)), a.gid, b.gid
-		from polygons a, polygons b
+			st_makeline(st_setSRID(st_centroid(a.geom),3857), st_setSRID(st_centroid(b.geom),3857)), a.gid, b.gid
+		from p_iller a, p_iller b
 		where st_intersects(a.geom, b.geom) and a.gid != b.gid
 		)
 	-- LIMITATION: This generates the same edge twice! Do NOT consider the symmetric relation between the polygons
